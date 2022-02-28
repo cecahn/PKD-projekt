4825a30773161fee0cf52d854676bd3c9c5a42e6
Nawwar <54795785+Nawwar02@users.noreply.github.com>
2022-02-28 16:21:45 +0100

    list of all words written in the current round of the game

--- projekt/projekt.hs
+++ projekt/projekt.hs
@@ -10,24 +10,32 @@ type Answer = [Char]
 
 
 listOfPoints = [('a',1), ('b',2), ('c',2), ('d',1), ('e',1), ('f',4), ('g',2), ('h',3), ('i',1), ('j',4), ('k',3), ('l',1), ('m',3), ('n',1), ('o',2), ('p',3), ('q',10), ('r',1), ('s',1), ('t',1), ('u',3), ('v',4), ('w',2), ('x',10), ('y',8), ('z',10)]
-tableOfPoints = Ta.fromList listOfPoints 
+tableOfPoints = Ta.fromList listOfPoints
 
+{-- main
+provides the game instruction for player and prints out the list of the letters which are avalible to use
+PRE: True
+RETURNS: 
+
+
+--}
 
 main :: IO ()
 main = do
+    putStrLn " "
     putStrLn "Welcome to Alphapet the computer game"
     putStrLn " "
     putStrLn "INSTRUCTIONS"
-    putStrLn "You will be given a list of 30 randomly chosen letters and you need to build as many words as possible using these letters." 
+    putStrLn "You will be given a list of 30 randomly chosen letters and you need to build as many words as possible using these letters."
     putStrLn "Each letter is worth a specific number of points so the longer words and the more difficult letters you use will generate a higher score."
     putStrLn "Whenever you don't want to play anymore or cannot build any more words, you can choose to quit the game and you will get all of your words and your final score presented."
     putStrLn "If you manage to use all of your letters you win the game."
     putStrLn " "
     putStrLn "RULES"
-    putStrLn "1. Words must be longer than one letter."
-    putStrLn "2. Each word must be a valid english word and therefore be included in the english dictionary file (english3.txt)."
+    putStrLn "1. Inputs must be longer than one letter."
+    putStrLn "2. Each input must be a valid english word and therefore be included in the english dictionary file (english3.txt)."
     putStrLn "3. You must only use letters from your given list."
-    putStrLn "4. Each new word must begin with the last letter of the previous word."
+    putStrLn "4. Each new input must begin with the last letter of the previous word."
     putStrLn " "
     putStrLn "Enter your name: "
     gamerName <- getLine
@@ -37,30 +45,29 @@ main = do
     putStrLn "Here are your letters: "
     print rm
     putStrLn " "
-    putStrLn "Do you want to continue? (yes/no): "
+    putStrLn "Do you want to continue? \"yes\"/\"no\": "
     continue <- readAnswer
-    if continue == "yes" then do 
+    if continue == "yes" then do
         putStrLn "Good luck!"
         putStrLn " "
-        continuePlay rm 0 '.'
+        continuePlay rm 0 '.' []
     else
       putStrLn "Okay, maybe next time!"
 
 
 {-- randomletters
-arranges 30 random letters and inserts them in a list
-RETURNS: 
-
+Arranges 30 random letters and inserts them in a list
+PRE: TRUE
+RETURNS: a string with 30 letters
+EXAMPLES: 
+  randomLetters == "rwpnxhbyqzhjdasqsrgwentzktldyy"
+  randomLetters == "hmsmewwiqjepqtgasvzciwucolkdqh"
 --}
---vowels = ['a', 'e', 'i', 'o', 'u']
---('b','z') = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+
 randomLetters :: IO [Char]
 randomLetters = fmap  (take 30 . randomRs ('a','z')) newStdGen
 
-randomNum :: IO [Int]
-randomNum = fmap  (take 10 . randomRs (0,4)) newStdGen
 
--- borrowed from Lab 15
 readAnswer :: IO Answer
 readAnswer =
   catch (do
@@ -71,18 +78,22 @@ readAnswer =
      putStrLn " "
      readAnswer) :: SomeException -> IO Answer)
 
-  
-continuePlay :: [Char] -> Int -> Char -> IO ()
-continuePlay list acc char = do
+
+
+continuePlay :: [Char] -> Int -> Char -> [[Char]] -> IO ()
+continuePlay list acc char wordlist = do
   english <- fmap lines $ readFile "english3.txt"
   word <- collectWord list char
   if word == "" then do
     putStrLn ""
   else do
-    if word `elem` english then do 
+    if word `elem` english then do
       let nextfirst = last word
       let newlist = delete (init word) list
       let score = pointscounter word acc
+      putStrLn " "
+      putStrLn "Here are all your words for this round: "
+      print (word : wordlist)
       putStrLn " "
       putStrLn "Here is your total score: "
       print score
@@ -93,12 +104,12 @@ continuePlay list acc char = do
       putStrLn "Here is the letter your next word has to begin with: "
       print nextfirst
       putStrLn " "
-      if (length newlist) == 1 then do
+      if length newlist == 1 then do
         putStrLn "You won, you used all letters from your list!"
       else do
-        putStrLn "Do you want to continue? (yes/no): "
+        putStrLn "Do you want to continue? (\"yes\"/\"no\") : "
         continue <- readAnswer
-        if continue == "yes" then continuePlay newlist (pointscounter word acc) nextfirst
+        if continue == "yes" then continuePlay newlist (pointscounter word acc) nextfirst (word : wordlist)
         else do
           putStrLn " "
           putStrLn "Thank you for playing!"
@@ -109,14 +120,47 @@ continuePlay list acc char = do
     else do
       putStrLn "Invalid input. Type a valid english word"
       putStrLn " "
-      continuePlay list acc char
+      continuePlay list acc char wordlist
 
-{-
+{- collectWord list char
+Takes an input from the user and checks if the word contains more than one letter and only contains letters
+  which the game has provided
+SIDE EFFECTS: reads in users input and prints out the game interface
+RETURN:  
+EXAMPLES: 
+  *Main> collectWord "abcdan" '.'
+  Enter a word: 
+  can
+  "can"
+
+  *Main> collectWord "abcdan" '.'
+  Enter a word: 
+  hey
+  Invalid input. Use letters from your list.
+ 
+  Enter a word: 
+
+  *Main> collectWord "abcd" '.'
+  Enter a word: 
+  c
+  "You lose for trying to cheat." 
+
+
+  *Main> collectWord "hcyaun" 'j'
+  Enter a word: 
+  can
+  You must use the last character from your previous word as the first letter of your current word.
+  That letter is: 
+  'j'
+ 
+  Enter a word: 
+
 
 -}
 
 collectWord :: [Char] -> Char -> IO [Char]
 collectWord list char = do
+    putStrLn " "
     putStrLn "Enter a word: "
     word <- getLine
     if (length word) == 0 then do
@@ -124,7 +168,7 @@ collectWord list char = do
       putStrLn " "
       collectWord list char
     else do
-      if length word == 1 then do
+      if (length word) == 1 then do
         putStrLn "You lose for trying to cheat."
         return ""
       else do
@@ -135,8 +179,8 @@ collectWord list char = do
             putStrLn "Invalid input. Use letters from your list."
             putStrLn " "
             collectWord list char
-        else 
-          if (head word) == char then do
+        else
+          if head word == char then do
             if letterchecker list (tail word) then
               return word
             else do
@@ -151,29 +195,57 @@ collectWord list char = do
             collectWord list char
 
 
-{-- letterchecker checks if the letters in the input matches the given ones
-RETURNS:
+{-- letterchecker (y:ys) (x:xs)
+checks if the letters in (x:xs) are included in (y:ys)
+PRE: True
+RETURNS: True if the letters (x:xs) are included in (y:ys), Retruns False otherwise
+EXAMPLES: 
+  letterchecker "abcdefn" "can" == True
+  letterchecker "abcdefn" "hey" == False
+  letterchecker "" "hello" == False
+  letterchecker "hello" "" == True
 
 --}
 letterchecker :: Eq a => [a] -> [a] -> Bool
+-- VARIANT: length (y:ys) or length (x:xs)
 letterchecker [] [] = True
 letterchecker (y:ys) [] = True
 letterchecker [] (x:xs) = False
 letterchecker (y:ys) (x:xs) = letterchecker (lettercheckerAux x (y:ys)) xs
 
+
+{-- lettercheckerAux x (y:ys)
+deletes the first x which is presented in (y:ys)
+PRE: True
+RETURNS: the rest of (y:ys) if x is an element in (y:ys), returns and empty string otherwise
+EXAMPLES: 
+  lettercheckerAux ' ' "hello" == ""
+  lettercheckerAux  'h' "hello" == "ello"
+  lettercheckerAux  'l' "hello" == "helo"
+  lettercheckerAux 'y' "who" == ""
+--}
 lettercheckerAux :: Eq a => a -> [a] -> [a]
+--VARIANT: length (y:ys)
 lettercheckerAux _ [] = []
-lettercheckerAux x (y:ys) = 
+lettercheckerAux x (y:ys) =
   if x `elem` (y:ys) then
     if x == y then ys
     else y : lettercheckerAux x ys
   else []
 
 
-{--delete deletes the letters of the input word from the original list
+{-- delete lst (y:ys)
+deletes the elemnt in (y:ys) which are included in lst 
+PRE: True
+RETURNS: the rest of (y:ys) after the common elements between (y:ys) and lst have been removed
+EXAMPLES:
+   delete "he" "hello" == "llo"
+   delete "hello" "he" == ""
+   delete "" "word" == "word"
 
 --}
 delete :: Eq a => [a] -> [a] -> [a]
+--VARIANT: lenght lst or length (y:ys)
 delete _ [] = []
 delete [] (y:ys) = y:ys
 delete lst (y:ys)
@@ -186,5 +258,5 @@ delete lst (y:ys)
 --}
 pointscounter :: [Char] -> Int -> Int
 pointscounter [] acc = acc
-pointscounter (x:xs) acc = (conv (Ta.lookup tableOfPoints x)) + (pointscounter xs acc)
-  where conv (Just x) = x 
+pointscounter (x:xs) acc = conv (Ta.lookup tableOfPoints x) + pointscounter xs acc
+  where conv (Just x) = x
